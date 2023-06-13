7790dfc2ea4a0ad5a8bf3773bc13ee6f5cde07e3
alvianzf <alvianzf@gmail.com>
2023-06-13 21:04:32 +0700

    add getprice-json endpoint

---
+++ routes/api/getprice-json.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const axios = require('axios');
+
+const data = axios.create({
+    baseURL: 'https://web.klikmbc.biz/json'
+});
+
+router.get('/', function(req, res) {
+    const { username, password, to, from, date } = req.query;
+
+    let missingParameter = "missing parameter: "
+    if (!username) missingParameter += " username;"
+    if (!password) missingParameter += " password;"
+    if (!to) missingParameter += " to;"
+    if (!from) missingParameter += " from;"
+    if (!date) missingParameter += " date;"
+
+    if (!username || !password || !to || !from || !date) {
+        return res.status(400).send({ error: missingParameter});
+    }
+
+    axios
+        .post(`getprice-json`, {
+            username,
+            password,
+            to,
+            from,
+            date,
+            adult: adult ?? 0,
+            child: child ?? 0,
+            infant: infant ?? 0
+        })
+        .then(response => {
+            res.send(response.data);
+        })
+        .catch(error => {
+            const {code, status} = error
+            res.status(500).send({code, status});
+        });
+});
+
+module.exports = router;
