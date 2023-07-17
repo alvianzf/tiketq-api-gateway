ed0f8996dc74c16528f553d17fd054265505c2e9
alvianzf <alvianzf@gmail.com>
2023-07-17 10:47:26 +0700

    add form data package

--- routes/api/resetpassword.js
+++ routes/api/resetpassword.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const axios = require('axios');
+const FormData = require('form-data');
 
 const data = axios.create({
     baseURL: 'https://web.klikmbc.biz/json'
